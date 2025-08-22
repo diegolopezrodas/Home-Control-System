9e3a9517cf2a1d582ee55532fa18951dad5373c8
Diego Lopez-Rodas <diegolopezrodas+github@gmail.com>
2025-08-22 18:59:59 -0400

    Add files via upload

---
+++ Web Interface/public/index.php
@@ -0,0 +1,24 @@
+<!-- **********************************************************************
+      CREATED BY    : Diego Lopez-Rodas
+      CREATION DATE : 08/01/2025
+      UPDATE DATE   : 08/22/2025
+      DESCRIPTION   : This is the front-end of the 'Home Control System'
+                      web application, that will include files to control,
+                      format, and connect
+     ********************************************************************** -->
+
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <title>Home Control System - Web App</title>
+  </head>
+
+  <body>
+    <hr color="blue">
+    <?php include('../private/form.php'); ?>
+  </body>
+
+</html>
+
