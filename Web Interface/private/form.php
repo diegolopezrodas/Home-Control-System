9237355399ecef6825074d408e7ceef75ff149ab
Diego Lopez-Rodas <diegolopezrodas+github@gmail.com>
2025-08-22 18:20:50 -0400

    Add files via upload

---
+++ Web Interface/private/form.php
@@ -0,0 +1,25 @@
+
+<!-- Beginning of Submission Form -->
+<div align="center">
+  <form method="POST" action="../private/Controller.php">
+    <table>
+      
+      <!-- Kitchen Light Field -->
+      <tr>
+        <td>Kitchen Light:</td>
+        <td>
+            <input type="checkbox" name="KitchenLightStatus" value="ON">
+        </td>
+      </tr>
+
+      <!-- Submit Buttons-->
+      <tr>
+        <td> </td>
+        <td>
+            <input type="submit" name="Submit"       value="Submit">&nbsp;
+            <input type="reset"  name="Clear Screen" value="Clear Screen">&nbsp;
+        </td>
+      </tr>
+    </table>
+  </form>
+</div>
