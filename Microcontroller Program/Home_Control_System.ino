2a5a849bd2269752dcfc60ded8bd8497315f39ee
Luis M. Lopez <luismlopez2004@gmail.com>
2025-08-23 15:46:37 -0400

    Update Home_Control_System.ino

--- Microcontroller Program/Home_Control_System.ino
+++ Microcontroller Program/Home_Control_System.ino
@@ -1,68 +1,59 @@
-const int kitchenLight = 13; //On-board LED for testing purposes
+const int kitchenLight = 5; //Kitchen Light Device
+const int onboardLED = 13;
 String incomingSerialMessage = "";
 
 void setup(){
+  Serial.begin(9600); //9600 baud rate for serial communication
+
   pinMode(kitchenLight, OUTPUT);
-  Serial.begin(9600); //Begins serial communication
+  pinMode(onboardLED, OUTPUT);
+
+  digitalWrite(onboardLED, LOW); //Ensures the onboard LED remains off after the microcontroller resets, and both the RX and TX lights go off
 }
 
 void loop(){ //Loops indefinitely
-  while(Serial.available() > 0){ //As long as there are bytes in the serial buffer, or as long as there is something to be read in the serial buffer
-    char currentSerialCharacter = Serial.read();
+  while (Serial.available() > 0){ //As long as there are bytes in the serial buffer, or as long as there is something to be read in the serial buffer
+    char currentSerialCharacter = Serial.read(); 
 
-    if(currentSerialCharacter == '\n'){ //If the current character in the serial message is the end-of-line indicator
+    if (currentSerialCharacter == '\n'){ //If the current character in the serial message is the end-of-line indicator
       incomingSerialMessage.trim(); //Removes any extra spaces or line breaks from the serial message
+      int spaceIndex = incomingSerialMessage.indexOf(" "); 
 
-      int spaceIndex = incomingSerialMessage.indexOf(" ");
-
-      if(spaceIndex > 0){ //A space is present after index 0
+      if (spaceIndex > 0){
         String device = incomingSerialMessage.substring(0, spaceIndex); //Obtains the name of the device from the serial message
         String command = incomingSerialMessage.substring(spaceIndex + 1); //Obtains the command from the serial message
 
-        serialMessageEvaluator(device, command);
+        evaluateCommand(device, command);
+      } 
+      else{ //No space was present, or the space was at the beginning
+        Serial.println("INVALID FORMAT");
       }
-      else{ //Either a space was not present, space index = -1, or the space was the first character in the serial message, space index = 0
-        Serial.println("Invalid Serial Message Format");
-      }
-
-      incomingSerialMessage = ""; //Clears the string so that it is empty for the next serial message, once we are done with the current serial message
-    }
+      incomingSerialMessage = ""; //Resets the incoming serial message 
+    } 
     else{
       incomingSerialMessage += currentSerialCharacter; //The serial message is stored character by character per iteration of this function
     }
   }
 }
 
-void serialMessageEvaluator(String device, String command){
-  device.toUpperCase();
-  command.toUpperCase();
+void evaluateCommand(String device, String command){ //To be expanded for other devices
+  device.toUpperCase(); //Formatting for device
+  command.toUpperCase(); //Formatting for command
 
-  if(device.equals("KITCHENLIGHT")){
-    if(command.equals("ON")){
-      digitalWrite(kitchenLight, HIGH); 
-      Serial.println("Kitchen Light " + command);
-    }
-    else if(command.equals("OFF")){
+  if (device.equals("KITCHENLIGHT")){
+    if (command.equals("ON")){
+      digitalWrite(kitchenLight, HIGH);
+      Serial.println("Kitchen Light is on.");
+    } 
+    else if (command.equals("OFF")){
       digitalWrite(kitchenLight, LOW);
-      Serial.println("Kitchen Light " + command);
-    }
+      Serial.println("Kitchen Light is off.");
+    } 
     else{
-      Serial.println("Invalid Command Selected: " + command);
+      Serial.println("INVALID COMMAND"); //The serial message was of valid format, but the command selected is not an available command
     }
-  }
-  else if(device.equals("PLACEHOLDERDEVICE")) //Repeat this else if block for the number of other devices to be controlled
-    if(command.equals("ON")){
-      //Turn device on
-      //Print device is on
-    }
-    else if(command.equals("OFF")){
-      //Turn device off
-      //Print device is off
-    }
-    else{
-      Serial.println("Invalid Command Selected: " + command);
-    }
+  } 
   else{
-    Serial.println("Unknown Device Selected: " + device); //The serial message was of valid format, but the device selected is not designated to be controlled
+    Serial.println("UNKNOWN DEVICE"); //The serial message was of valid format, but the device selected is not designated to be controlled
   }
 }
